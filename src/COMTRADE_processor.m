1f608920f22e9c58d143b8144d97e31a519e35f8
Gabriel Fernandes Rosa Bojikian <gabriel.bojikian.dev@gmail.com>
2021-08-01 20:24:42 -0300

    Feature: File reading complete
    Completed the COMTRADE file reading feature, as outlined in step 1 of
    the assignment. The README has been updated accordingly.
    
    This program can read the COMTRADE files and adjust the phase voltages
    and the phase currents to match the secondary values in their respective
    transformers.

--- src/COMTRADE_processor.m
+++ src/COMTRADE_processor.m
@@ -1,2 +1,66 @@
 clear all;
+close all;
 clc;
+
+% Choose the name of the file to be analysed
+filename = 'AD_GR_Rf25_F2_BCT_0';
+
+% Data input
+COMTRADE_data = csvread([filename '.dat']);
+config = csvread([filename '.cfg']);
+
+n = COMTRADE_data(:,1);
+ta = COMTRADE_data(:,2)*1E-6;
+fa_comtrade = 1/(ta(2) - ta(1));
+
+% Separating the channels, recording them in engineering values
+VANs = COMTRADE_data(:, 3)*config(3, 6) + config(3, 7);
+VBNs = COMTRADE_data(:, 4)*config(4, 6) + config(4, 7);
+VCNs = COMTRADE_data(:, 5)*config(5, 6) + config(5, 7);
+IALp = (COMTRADE_data(:, 6)*config(6, 6) + config(6, 7))*1E3;
+IBLp = (COMTRADE_data(:, 7)*config(7, 6) + config(7, 7))*1E3;
+ICLp = (COMTRADE_data(:, 8)*config(8, 6) + config(8, 7))*1E3;
+
+% Converting primary currents to secondary currents
+IALs = IALp / 1000;
+IBLs = IBLp / 1000;
+ICLs = ICLp / 1000;
+
+% Plotting the three phase voltages
+figure;
+plot(ta, VANs, ta, VBNs, ta, VCNs);
+grid;
+legend('VANs', 'VBNs', 'VCNs');
+title('Secondary voltages');
+xlabel('Time [s]');
+ylabel('Voltages [V]');
+
+% Plotting the three secondary line currents
+figure;
+plot(ta, IALs, ta, IBLs, ta, ICLs);
+grid;
+legend('IALs', 'IBLs', 'ICLs');
+title('Secondary currents');
+xlabel('Time [s]');
+ylabel('Currents [A]');
+
+% Simulating the filter
+pkg load signal;
+
+[nf, wc] = buttord(2*pi*90, 2*pi*60*8, 0.1, 36, 's');
+[num, den] = butter(nf, wc, 's');
+
+% Esta funcao gera o filtro de fato
+lpf = tf(num, den);
+
+% Generate an analog signal that will be processed by the lowpass filter
+VIALs_cond = IALs * (1/4);
+VIALs_fil = lsim(lpf, VIALs_cond, ta);
+% Plotting the A phase current after conditioning and filtering
+figure;
+plot(ta, VIALs_cond, ta, VIALs_fil); 
+grid;
+legend('VIALs_cond', 'VIALs_fil');
+title('Channel voltages inside the IED before and after the LPF');
+xlabel('Time [s]');
+ylabel('Voltage [V]');
